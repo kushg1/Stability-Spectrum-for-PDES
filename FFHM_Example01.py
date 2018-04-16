f8a59818f9092a89f0552ddcc41e82fc6680d5ef
rbushling <38433259+rbushling@users.noreply.github.com>
2018-04-16 16:41:15 -0700

    Added a crappy first file.

---
+++ FFHM_Example01.py
@@ -0,0 +1,22 @@
+import numpy, cmath
+
+
+def frange(start, stop, step):
+    i = start
+    while i < stop:
+        yield i
+        i += step
+
+# number of Fourier modes
+N = 16
+#number of Floquet modes
+D = 100
+
+c = numpy.zeros((2*N+1, 2*N+1), dtype=numpy.complex_)
+
+for d in range(0,D):
+    mu = 2.0*d/D
+    for k in range(0,2*N+1):
+        m = k-N
+        c[k,k] = (1j*(mu + 2*m))**2
+    print('eigs for mu = ' + str(mu) + ': ' + str(numpy.linalg.eig(c)))
