bbf547ae4506ae6588fa5d0739a9cd7ecba8aca6
Kush Gupta <bluek7@cs.washington.edu>
2018-05-22 09:46:57 -0700

    weierstrass with mpmath

--- weierstrass_ellip_library.py
+++ weierstrass_ellip_library.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.integrate as integrate
 import scipy.special
 import cmath
-# from mpmath import sqrt, mpc, sin, ellipfun, mpf
+from mpmath import sqrt, mpc, sin, ellipfun, mpf
 import scipy
 
 def frange(start, stop, step):
@@ -31,68 +31,94 @@ def weierstrass_to_jacobi(z, g2, g3):
     e_vec = weierstrass_Es(g2, g3)
     return (weier_to_jacobi_y(z, e_vec[0], e_vec[2]), weier_to_jacobi_k(e_vec[0], e_vec[1], e_vec[2]))
 
+sn = ellipfun('sn')
+cn = ellipfun('cn')
+dn = ellipfun('dn')
 
 results = []
 
-for g2 in frange(1.0,1.6,0.1):
-    r = []
-    for g3 in frange(-0.5,0.6,0.1):
-        weier_Es = weierstrass_Es(g2, g3)
-        w1 = omega1(g2, g3, weier_Es[0])
-        w3 = omega3(g2, g3, weier_Es[2])
-        k_val = weier_to_jacobi_k(weier_Es[0], weier_Es[1], weier_Es[2])
-        r.append([g2, g3, weier_Es[0], weier_Es[1], weier_Es[2], w1, w3, k_val])
-    results.append(r)
+# for g2 in frange(1.0,1.6,0.1):
+#     r = []
+#     for g3 in frange(-0.5,0.6,0.1):
+#         weier_Es = weierstrass_Es(g2, g3)
+#         w1 = omega1(g2, g3, weier_Es[0])
+#         w3 = omega3(g2, g3, weier_Es[2])
+#         k_val = weier_to_jacobi_k(weier_Es[0], weier_Es[1], weier_Es[2])
+#         r.append([g2, g3, weier_Es[0], weier_Es[1], weier_Es[2], w1, w3, k_val])
+#     results.append(r)
 
 # TODO: WORKS IF e1, e2, e3 ARE REAL
 def P(e1, e2, e3, z):
     Delta = 16 * ((e2 - e3) * (e3 - e1) * (e1 - e2)) ** 2
-    m = np.real((e2 - e3) / (e1 - e3))
+    m = (e2 - e3) / (e1 - e3) # np.real((e2 - e3) / (e1 - e3))
+    print(Delta)
     if Delta > 0:
+        m = (e2 - e3) / (e1 - e3)
         # TODO: Shouldn't necessarily be real
-        zs = np.real(np.lib.scimath.sqrt(e1 - e3) * z)
+        # zs = np.real(np.lib.scimath.sqrt(e1 - e3) * z)
+        zs = np.lib.scimath.sqrt(e1 - e3) * z
         # print(type(zs))
         # print(m)
-        ellip = scipy.special.ellipj(zs, m)
-        sn = ellip[0]
-        retval = e3 + (e1 - e3) / sn**2
+        # ellip = scipy.special.ellipj(zs, m)
+        # Sn = ellip[0]
+        Sn = sn(zs, m)
+        retval = e3 + (e1 - e3) / Sn**2
     elif Delta < 0:
-        H2 = 2 * (e2 ** 2) + e1 * e3
+        # print(Delta)
+        H2 = np.lib.scimath.sqrt(2 * (e2 ** 2) + e1 * e3)
+        m = 0.5 - (3 * e2) / (4 * H2)
         zp = 2 * z * np.lib.scimath.sqrt(H2)
-        ellip = scipy.special.ellipj(zp, m)
-        cn = ellip[1]
-        retval = e2 + H2 * (1 + cn) / (1 - cn)
+        # ellip = scipy.special.ellipj(zp, m)
+        # cn = ellip[1]
+        Cn = cn(zp, m)
+        retval = e2 + H2 * (1 + Cn) / (1 - Cn)
     
     return retval
 
 # TODO: WORKS IF e1, e2, e3 ARE REAL
 def PPrime(e1, e2, e3, z):
     Delta = 16 * ((e2 - e3) * (e3 - e1) * (e1 - e2)) ** 2
-    m = np.real((e2 - e3) / (e1 - e3))
+    # m = (e2 - e3) / (e1 - e3) # np.real((e2 - e3) / (e1 - e3))
     if Delta > 0:
+        m = (e2 - e3) / (e1 - e3)
         zs = np.lib.scimath.sqrt(e1 - e3) * z
-        ellip = scipy.special.ellipj(np.real(zs), m)
-        sn, cn, dn = ellip[0], ellip[1], ellip[2]
-        retval = -2 * (np.lib.scimath.sqrt((e1 - e3) ** 3)) * cn * dn / (sn ** 3)
+        # ellip = scipy.special.ellipj(np.real(zs), m)
+        # Sn, Cn, Dn = ellip[0], ellip[1], ellip[2]
+        Sn = sn(zs, m)
+        Cn = cn(zs, m)
+        Dn = dn(zs, m)
+
+        retval = -2 * (np.lib.scimath.sqrt((e1 - e3) ** 3)) * Cn * Dn / (Sn ** 3)
     elif Delta < 0:
         H2 = 2 * (e2 ** 2) + e1 * e3
+        m = 0.5 - (3 * e2) / (4 * H2)
         zp = 2 * z * np.lib.scimath.sqrt(H2)
-        ellip = scipy.special.ellipj(np.real(zp), m)
-        sn, cn, dn = ellip[0], ellip[1], ellip[2]
-        retval = -4 * (np.lib.scimath.sqrt(H2 ** 3)) * sn * dn / ((1 - cn) ** 2)
+        # ellip = scipy.special.ellipj(np.real(zp), m)
+        # sn, cn, dn = ellip[0], ellip[1], ellip[2]
+
+        Sn = sn(zp, m)
+        Cn = cn(zp, m)
+        Dn = dn(zp, m)
+
+        retval = -4 * (np.lib.scimath.sqrt(H2 ** 3)) * Sn * Dn / ((1 - Cn) ** 2)
 
     return retval
 
-# def main():
-#     z = 1
-#     g2 = 1
-#     g3 = 0.1
-#     weier = weierstrass_Es(g2, g3)
-#     print(weier)
-#     p = P(weier[0], weier[1], weier[2], z)
-#
-#     print(p)
+def main():
+    z = 4
+    g2 = 1
+    g3 = 1
+    weier = weierstrass_Es(g2, g3)
+    o1 = omega1(g2, g3, weier[0])
+    o3 = omega3(g2, g3, weier[2])
+    print(weier)
 
+    print(o1)
+    print(o3)
+    p = P(weier[0], weier[1], weier[2], z)
+    pp = PPrime(weier[0], weier[1], weier[2], z)
+    print("P: " + str(p))
+    print("PPrime: " + str(pp))
 
     # results = []
     # for g2 in frange(1.0,1.6,0.1):
@@ -143,5 +169,5 @@ def PPrime(e1, e2, e3, z):
     #         rownum += 1
     #     # print(rownum)
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
