252b6d3246da517030f989ddc8e84e232d11347f
rbushling <38433259+rbushling@users.noreply.github.com>
2018-05-22 11:33:04 -0700

    Looking for bugs

--- mKdV/mKdV_spectrum_01.py
+++ mKdV/mKdV_spectrum_01.py
@@ -28,7 +28,7 @@ cn = lambda y, k: scipy.special.ellipj(y, k**2)[1]
 dn = lambda y, k: scipy.special.ellipj(y, k**2)[2]
 
 # Use this code to plot Figure 14
-# k = 0.8
+# k = 0.85
 # mult_factor = np.sqrt(V/(2*k**2-1))
 # U = lambda y: k * mult_factor * cn(mult_factor*y, k)
 # U_prime = lambda y: -k * mult_factor**2 * sn(mult_factor*y, k) * dn(mult_factor*y, k)
@@ -38,9 +38,19 @@ dn = lambda y, k: scipy.special.ellipj(y, k**2)[2]
 # Use this code to plot Figure 10
 k = 1.8
 mult_factor = np.sqrt(V/(2*k**2-1))
-U = lambda y: k * mult_factor * dn(k*mult_factor*y, 1/k)
-U_prime = lambda y: -k**4 * mult_factor**2 * sn(k*mult_factor*y, 1/k) * cn(k*mult_factor*y, 1/k)
-L = 4*scipy.special.ellipk((k**2) / (1 - k**2)) / (np.sqrt(k**2 - 1) * mult_factor)
+U = lambda y: k * mult_factor * dn(k*mult_factor*y, 1/k**2)
+U_prime = lambda y: -k**4 * mult_factor**2 * sn(k*mult_factor*y, 1/k**2) * cn(k*mult_factor*y, 1/k**2)
+L = 2*scipy.special.ellipk(1/k**2) / (k * mult_factor)
+
+print(L)
+
+# Case of C=0 but U takes an imaginary argument
+# k = 0.2
+# mult_factor = np.sqrt(V/(2*k**2-1))
+# U = lambda y: k * mult_factor / cn(np.real(-1j*mult_factor*y), np.sqrt(1-k**2))
+# U_prime = lambda y: -1j * k * mult_factor**2 * sn(np.real(-1j*mult_factor*y), np.sqrt(1-k**2)) * \
+#                     dn(np.real(-1j*mult_factor*y), np.sqrt(1-k**2)) / cn(np.real(-1j*mult_factor*y), np.sqrt(1-k**2))**2
+# L = 4*scipy.special.ellipk(k**2)/mult_factor
 
 # fact = lambda y: weier.P(e1, e2, e3, 0.5*(y+y0)) - V/3
 # denom = lambda y: (fact(y) - 2*np.sqrt(-2*E)) * (fact(y) + 2*np.sqrt(-2*E))
@@ -92,6 +102,38 @@ f_hats = np.array([fourier_coeffs(f3, 2*N, L), fourier_coeffs(f2, 2*N, L), fouri
                    fourier_coeffs(f0, 2*N, L)])
 evals = np.array([], dtype=np.complex_)
 
+fourier_U_coeffs = fourier_coeffs(U, 2*N, L)
+for k in range(-N,N+1,1):
+    print('n = ' + str(k) + ': ' + str(fourier_U_coeffs[k]))
+
+fourier_U = lambda x: sum([fourier_U_coeffs[i] * np.exp(2*cmath.pi*1j*i*x/L) for i in range(-N,N+1,1)])
+
+plt.figure(1)
+plt.scatter([a for a in frange(-3,3,0.01)], [fourier_U(k) for k in frange(-3,3,0.01)], marker='.')
+plt.show()
+
+
+
+
+xs = []
+ys = []
+#
+# pts = []
+#
+# for e in f_hats[2]:
+#     a = abs(e)
+#     b = np.log(a)
+#     pts = np.append(pts, b)
+#
+#
+# the_range = range(-N,len(pts)-N)
+# plt.figure(0)
+# plt.plot(the_range, pts, marker='.')
+# plt.ylim([-50, 50])
+# plt.show()
+
+
+
 for mu in frange(-cmath.pi/L, cmath.pi/L, 2*cmath.pi/(L*D)):
     L_matrix = np.zeros((2*N + 1, 2*N + 1), dtype=np.complex_)
     for n in range(-N, N+1):
@@ -99,16 +141,28 @@ for mu in frange(-cmath.pi/L, cmath.pi/L, 2*cmath.pi/(L*D)):
             factor = np.array([(1j*(mu + 2*cmath.pi*m/L))**(3-p) for p in range(0,4)], dtype=np.complex_)
             idx = 2*N+(n-m)
             L_matrix[n+N, m+N] = np.dot(f_hats[:,idx], factor)
-    evals = np.append(evals, np.linalg.eigvals(L_matrix))
+    eigs = np.linalg.eigvals(L_matrix)
+    for e in eigs:
+        xs = np.append(xs, mu)
+        ys = np.append(ys, np.imag(e))
+        # points = np.append(points, (mu, e))
+
+    evals = np.append(evals, eigs)
+
 
 plt.figure(1)
+plt.scatter(xs, ys, marker='.')
+plt.ylim([-50, 50])
+plt.show()
+
+plt.figure(2)
 plt.scatter(evals.real, evals.imag, color=(0.05,0.75,0.5), marker='.')
-plt.xlim([-150, 150])
+plt.xlim([-80, 80])
 plt.ylim([-200, 200])
 plt.show()
 
-plt.figure(2)   # consider adding horizontal lines and stretching the vertical axis to accentuate the "actual period"
+plt.figure(3)   # consider adding horizontal lines and stretching the vertical axis to accentuate the "actual period"
 plt.plot([y for y in frange(0., 50., 0.1)], [U(y) for y in frange(0., 50., 0.1)], 'k-')
 plt.xlim([0, 50])
-plt.ylim([0, 4])
+plt.ylim([-15, 15])
 plt.show()
